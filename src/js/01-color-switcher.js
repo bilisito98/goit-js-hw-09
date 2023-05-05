a050aee504a6343030393c4086e2b8940a1249f3
william <wilsher987@gmail.com>
2023-05-04 22:32:48 -0500

    thirt commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,29 @@
+const buttonStart = document.querySelector('[data-start]');
+const buttonStop = document.querySelector('[data-stop]');
+const body = document.querySelector('body');
 
+let timerId = null;
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+const cambioColor = () => {
+  timerId = setTimeout(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+    cambioColor();
+  }, 1000)
+  buttonStart.setAttribute('disabled', true);
+  buttonStop.removeAttribute('disabled')
+}
+
+const pararColor = () => {
+  clearTimeout(timerId)
+  buttonStart.removeAttribute('disabled');
+  buttonStop.setAttribute('disabled', true);
+}
+
+buttonStart.addEventListener('click', cambioColor);
+buttonStop.addEventListener('click', pararColor);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,36 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', event => {
+  event.preventDefault();
+
+  let delay = Number(event.target.elements.delay.value);
+  let step = Number(event.target.elements.step.value);
+  let amount = Number(event.target.elements.amount.value);
+
+
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay = delay+step
   }
-}
+});
+
+
+
+const createPromise = (position, delay) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+};
